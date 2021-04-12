3f7bed108a706608c2772163d565e49002eefbd3
ruhulaminafp <ruhul.530025@gmail.com>
2021-04-12 06:18:59 +0600

    Add files via upload

---
+++ NewBookEntry.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="NewBookEntry.aspx.cs" Inherits="ACCCounter.NewBookEntry" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="form-horizontal clearfix" runat="server">
+        <div class="panel panel panel-primary">
+            <div class="panel-heading">
+                <h4>New Book Entry</h4>
+            </div>
+            <div class="panel-body">
+                <div class="form-group">
+                    <asp:Label AssociatedControlID="txtBookId" runat="server" CssClass="control-label col-md-2 no-padding-right">Book Id</asp:Label>
+                    <div class="col-md-4">
+                        <asp:TextBox ID="txtBookId" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label AssociatedControlID="txtBookName" runat="server" CssClass="control-label col-md-2 no-padding-right">Book Name</asp:Label>
+                    <div class="col-md-4">
+                        <asp:TextBox ID="txtBookName" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label AssociatedControlID="ddlBookType" CssClass="control-label col-md-2 no-padding-right" runat="server">Book Type</asp:Label>
+                    <div class="col-md-4">
+                        <asp:DropDownList ID="ddlBookType" CssClass="form-control" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label AssociatedControlID="ddlClass" CssClass="control-label col-md-2 no-padding-right" runat="server">Class</asp:Label>
+                    <div class="col-md-4">
+                        <asp:DropDownList ID="ddlClass" CssClass="form-control" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <asp:Label AssociatedControlID="ddlCompany" CssClass="control-label col-md-2 no-padding-right" runat="server">Company</asp:Label>
+                    <div class="col-md-4">
+                        <asp:DropDownList ID="ddlCompany" CssClass="form-control" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-4 col-md-offset-2">
+                        <asp:Button runat="server" CssClass="btn btn-success" ID="btnSave" Text="Save" OnClick="btnSave_Click"></asp:Button>
+                        <asp:Button runat="server" CssClass="btn btn-primary" ID="btnUpdate" Text="Update" OnClick="btnUpdate_Click"></asp:Button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <asp:Label AssociatedControlID="txtSearch" runat="server" CssClass="control-label col-md-6 no-padding-right">Search</asp:Label>
+                            <div class="col-md-4">
+                                <asp:TextBox ID="txtSearch" CssClass="form-control" runat="server" OnTextChanged="txtSearch_TextChanged" AutoPostBack="true"></asp:TextBox>                         
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="table-responsive  col-md-12">
+                    <asp:GridView ID="gvwBookEntry" CssClass="table-responsive" runat="server" Align="Center" AutoGenerateColumns="false" AllowPaging="true" OnPageIndexChanging="gvwBookEntry_PageIndexChanging" OnRowDataBound="gvwBookEntry_RowDataBound" OnSelectedIndexChanged="gvwBookEntry_SelectedIndexChanged">
+                        <PagerSettings Mode="NumericFirstLast"/>
+                        <Columns>
+                            <asp:CommandField SelectText="Select" ShowSelectButton="true" ItemStyle-CssClass="HiddenColumn"
+                                HeaderStyle-CssClass="HiddenColumn" />
+                            <asp:BoundField HeaderStyle-Width="200px" DataField="BookID" HeaderText="BookID" />
+                            <asp:BoundField HeaderStyle-Width="350px" DataField="BookName" HeaderText="BookName" />
+                            <asp:BoundField HeaderStyle-Width="200px" DataField="Code" HeaderText="Code" />
+                            <asp:BoundField HeaderStyle-Width="200px" DataField="ClassName" HeaderText="ClassName" SortExpression="DateField" />
+                            <asp:BoundField HeaderStyle-Width="200px" DataField="BookType" HeaderText="BookType" />
+                            <asp:BoundField HeaderStyle-Width="200px" DataField="CompanyName" HeaderText="CompanyName" />
+                        </Columns>
+                        <SelectedRowStyle BackColor="#393737" Font-Bold="true" ForeColor="#F7F7F7" />
+                    </asp:GridView>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="RequiredJS" runat="server">
+    <script>
+        $(document).ready(function () {
+            open_menu("Book Informations");
+        });
+    </script>
+</asp:Content>
---
+++ NewBookEntry.aspx.cs
@@ -0,0 +1,247 @@
+﻿using BLL;
+using QuickSell;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ACCCounter
+{
+    public partial class NewBookEntry : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if(!IsPostBack)
+                {
+                    txtBookId.Enabled = true;
+
+                    loadBookInformationToGrid();
+                    btnUpdate.Enabled = false;
+                    LoadBookType();
+                    LoadClass();
+                    LoadSupplierCompany();
+                }
+            }
+            catch (Exception)
+            {
+                 
+            }
+        }
+
+        private void LoadBookType()
+        {
+            LoadComboData loadClass = new LoadComboData();
+            loadClass.BookType(ddlBookType);
+        }
+
+        private void LoadClass()
+        {
+            ddlClass.DataSource = Li_ClassesManager.GetAllLi_Classess();
+            ddlClass.DataTextField = "ClassName";
+            ddlClass.DataValueField = "ClassID";
+            ddlClass.DataBind();
+            ddlClass.Items.Insert(0, new ListItem("--select--", ""));
+        }
+
+        private void LoadSupplierCompany()
+        {
+            li_CompanyManager company=new li_CompanyManager();
+            ddlCompany.DataSource =  company.GetAllCompany().Tables[0];
+            ddlCompany.DataTextField = "CompanyName";
+            ddlCompany.DataValueField = "CompanyId";
+            ddlCompany.DataBind();
+            ddlCompany.Items.Insert(0, new ListItem("--select", ""));
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+
+                li_BookInformation _bookInformation = new li_BookInformation();
+
+                _bookInformation.BookName = txtBookName.Text;
+
+                _bookInformation.ClassID = Int32.Parse(ddlClass.SelectedValue.ToString());
+                _bookInformation.CreatedBy = int.Parse(Session["UserID"].ToString());
+                _bookInformation.CreatedDate = DateTime.Now;
+                _bookInformation.ModifiedBy = int.Parse(Session["UserID"].ToString());
+                _bookInformation.ModifiedDate = DateTime.Now;
+                _bookInformation.BookType = int.Parse(ddlBookType.SelectedValue.ToString());
+
+                object bookRow = getTotalBookRow();
+
+                string nextID = bookRow.ToString();
+
+                string BookNewID = txtBookId.Text;           
+
+                _bookInformation.BookID = BookNewID;
+                _bookInformation.CompanyId = int.Parse(ddlCompany.SelectedValue);
+                _bookInformation.CompName = ddlCompany.SelectedValue.ToString();
+
+                li_BookInformationManager.Insert_BookInformation(_bookInformation);
+
+                loadBookInformationToGrid();
+                ClearAll();
+
+                string message = "Saved successfully.";
+                string script = "window.onload = function(){ alert('";
+                script += message;
+                script += "');";
+                script += "window.location = '";
+                script += Request.Url.AbsoluteUri;
+                script += "'; }";
+                //  ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.Page.GetType(), "SuccessMessage", script, true);
+
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+
+        public static int getTotalBookRow()
+        {
+            DataAccessObject dsa = new DataAccessObject();
+            using (SqlConnection connection = new SqlConnection(dsa.ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SMPM_GetNext_Book", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@lastin", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                connection.Open();
+
+                int result = cmd.ExecuteNonQuery();
+
+                return (int)cmd.Parameters["@lastin"].Value;
+            }
+        }
+
+        private void loadBookInformationToGrid()
+        {
+            try
+            {
+                gvwBookEntry.DataSource = li_BookInformationManager.GetAll_BookInformations(string.Empty).Tables[0];
+                gvwBookEntry.DataBind();
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void ClearAll()
+        {
+            // cmbPublisherComName.SelectedIndex = -1;
+            ddlClass.SelectedIndex = -1;
+            ddlBookType.SelectedIndex = -1;
+            txtBookName.Text = "";
+        }
+
+        protected void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                gvwBookEntry.DataSource = li_BookInformationManager.GetAll_BookInformationsByBookName(txtSearch.Text).Tables[0];
+                gvwBookEntry.DataBind();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected void gvwBookEntry_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            try
+            {
+                gvwBookEntry.PageIndex = e.NewPageIndex;
+                gvwBookEntry.DataSource = li_BookInformationManager.GetAll_BookInformations(string.Empty).Tables[0];
+                gvwBookEntry.DataBind();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                li_BookInformation _bookInformation = new li_BookInformation();
+
+                _bookInformation.BookID = txtBookId.Text;
+                _bookInformation.BookName = txtBookName.Text;
+                _bookInformation.ClassID = Int32.Parse(ddlClass.SelectedValue.ToString());
+                _bookInformation.ModifiedBy = int.Parse(Session["UserID"].ToString());
+                _bookInformation.ModifiedDate = DateTime.Now;
+                _bookInformation.BookType = int.Parse(ddlBookType.SelectedValue.ToString());
+                _bookInformation.CompanyId = int.Parse(ddlCompany.SelectedValue);
+
+                li_BookInformationManager.Update_BookInformation(_bookInformation);
+
+                loadBookInformationToGrid();
+                ClearAll();
+
+                string message = "Update successfully.";
+                string script = "window.onload = function(){ alert('";
+                script += message;
+                script += "');";
+                script += "window.location = '";
+                script += Request.Url.AbsoluteUri;
+                script += "'; }";
+                //  ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
+                ScriptManager.RegisterClientScriptBlock(this.Page, this.Page.GetType(), "SuccessMessage", script, true);
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            txtBookId.Enabled = true;
+        }
+
+        protected void gvwBookEntry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                txtBookId.Enabled = false;
+                GridViewRow row = gvwBookEntry.SelectedRow;
+                string BookId = row.Cells[1].Text;
+
+               DataTable dtgvw= li_BookInformationManager.GetAll_BookInformations(BookId).Tables[0];
+
+               txtBookId.Text = dtgvw.Rows[0]["BookID"].ToString();
+               txtBookName.Text = dtgvw.Rows[0]["BookName"].ToString();
+               ddlClass.SelectedValue = dtgvw.Rows[0]["ClassID"].ToString();
+               ddlBookType.SelectedValue = dtgvw.Rows[0]["BookTypeID"].ToString();
+               ddlCompany.SelectedValue = dtgvw.Rows[0]["CompanyId"].ToString();
+
+               btnUpdate.Enabled = true;
+               btnSave.Enabled = false;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        protected void gvwBookEntry_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+                e.Row.Attributes.Add("onClick", Page.ClientScript.GetPostBackEventReference(sender as GridView, "Select$" + e.Row.RowIndex.ToString()));
+        }
+    }
+}
